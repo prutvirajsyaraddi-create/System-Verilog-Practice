add8ee480a02add9e728a1640242721f32c7d671
prutvirajsyaraddi-create <prutviraj.s.yaraddi@gmail.com>
2026-09-04 18:04:51 +0530

    Delete SRC directory

--- SRC/SRC/or_gate.sv
+++
@@ -1,8 +0,0 @@
-module or_gate(
-  input a,b,
-  output logic y
-);
-  always @(*)begin
-    y=a|b;
-  end
-endmodule
--- SRC/and_gate.sv
+++
@@ -1,8 +0,0 @@
-module and_gate(
-  input a,b,
-  output reg y
-          );
-  always @(*)begin
-    y=a&b;
-  end
-  endmodule
