c60c1ded3214dcccacced0e513f2e9bbe02d84ef
prutvirajsyaraddi-create <prutviraj.s.yaraddi@gmail.com>
2026-09-05 11:08:39 +0530

    Modify mux_2 to handle 4-bit inputs and outputs

--- SRC/mux_2:1.sv
+++ SRC/mux_2:1.sv
@@ -1,6 +1,7 @@
 module mux_2(
-  input logic d0,d1,sel,
-  output logic y);
+  input logic [3:0]d0,d1,
+  input logic sel,
+  output logic [3:0]y);
   always_comb begin
   assign y=sel?d1:d0;
   end
